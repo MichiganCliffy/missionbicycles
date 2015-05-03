79d5adcdd4428c5de1402aa1f7ac5712475d0847
jdalton308 <jdalton308@yahoo.com>
2015-05-02 23:53:15 -0700

    Finished slider and controls

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var bikesApp = angular.module('bikesApp', [
 	'ngTouch'
 ]);
 
-bikesApp.controller('mainController', ['$scope', '$http', function($scope, $http){
+bikesApp.controller('mainController', ['$scope', '$http', '$window', function($scope, $http, $window){
 
 	// Get catalogue data
 	// If a multi-page app, would put this in a service
@@ -13,12 +13,57 @@ bikesApp.controller('mainController', ['$scope', '$http', function($scope, $http
 			console.log('Data recieved:');
 			console.log(data);
 
-			$scope.catalogue = data;
+			// Break-up each model name into 2 properties
+
+			$scope.catalog = data;
+			setSlider($scope.catalog);
 		}).
 		error(function(data, status){
 			console.error('Error retrieving data: '+ status);
 		});
 
-	
+
+	// SLIDER
+	//================
+	// If a larger app, would make own directive
+
+	var setSlider = function(catalog){
+
+		// First, set container width
+		$scope.itemNumber = catalog.products.length;
+		$scope.itemWidth = $window.innerWidth * 0.8; // Set image width to 80% of window
+		var initialPos = $window.innerWidth * 0.1; // Center slider by moving left 10%;
+		var sliderWidth = $scope.itemWidth * $scope.itemNumber;
+
+		angular.element('.slider').css({'width': sliderWidth, 'left':initialPos});
+	};
+
+	// Set control actions
+	$scope.slidePosition = 0;
+
+	$scope.next = function() {
+		if ($scope.slidePosition < $scope.itemNumber-1) {
+			angular.element('.slider').css('left', '-='+$scope.itemWidth);
+			$scope.slidePosition++;
+		}
+	};
+
+	$scope.prev = function() {
+		if ($scope.slidePosition > 0) {
+			angular.element('.slider').css('left', '+='+$scope.itemWidth);
+			$scope.slidePosition--;
+		}
+	};
+
+	$scope.goToSlide = function(target) {
+		var slideTarget = target - 1;
+		var slideChange = slideTarget - $scope.slidePosition;
+		var slideDist = slideChange * $scope.itemWidth;
+
+		angular.element('.slider').css('left', '-='+slideDist);
+		$scope.slidePosition = slideTarget;
+	};
+
+
 
 }]);
